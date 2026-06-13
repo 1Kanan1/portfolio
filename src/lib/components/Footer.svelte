7a4dcf6319997da24618879bfae8b8a06854a5e4
1Kanan1 <83110215+1Kanan1@users.noreply.github.com>
2026-06-13 17:55:36 +0400

    chore: update Footer copyright name

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="mt-24 pt-8 border-t border-slate-200">
-	<p class="text-sm text-slate-400">
-		&copy; {new Date().getFullYear()} John Doe. Minimalist portfolio.
-	</p>
+    <p class="text-sm text-slate-400">
+        &copy; {new Date().getFullYear()} 1Kanan1. Minimalist portfolio.
+    </p>
 </footer>
